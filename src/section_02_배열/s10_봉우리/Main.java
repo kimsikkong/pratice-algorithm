146eea14fba471eb269a2d730e91d0dc7873e3e7
김홍식 <kimsikkong@gmail.com>
2021-04-11 21:11:18 +0900

    봉우리 문제 풀이 추가

---
+++ src/section_02_배열/s10_봉우리/Main.java
@@ -0,0 +1,43 @@
+package section_02_배열.s10_봉우리;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int n, int[][] arr) {
+        int count = 0;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                int colMax = Math.max(arr[i][j - 1], arr[i][j + 1]);
+                int rowMax = Math.max(arr[i - 1][j], arr[i + 1][j]);
+                if (arr[i][j] > Math.max(colMax, rowMax)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] arr = new int[n + 2][n + 2];
+        for (int i = 0; i < n + 2; i++) {
+            for (int j = 0; j < n + 2; j++) {
+                if (i == 0 || i == n + 1 || j == 0 || j == n + 1) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        Main T = new Main();
+        System.out.println(T.solution(n, arr));
+    }
+}
