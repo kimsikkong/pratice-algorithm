8d974e5b3c8e50b17acc6197b6e516d296421e3b
김홍식 <kimsikkong@gmail.com>
2021-04-20 22:43:49 +0900

    매출액의 종류 문제 풀이 추가

---
+++ src/section_04_해쉬_정렬/s03_매출액의_종류/Main.java
@@ -0,0 +1,42 @@
+package section_04_해쉬_정렬.s03_매출액의_종류;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    public ArrayList<Integer> solution(int n, int k, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < k - 1; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        int lt = 0;
+        for (int rt = k - 1; rt < n; rt++) {
+            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+            answer.add(map.keySet().size());
+            map.put(arr[lt], map.get(arr[lt]) - 1);
+            if (map.get(arr[lt]) == 0) map.remove(arr[lt]);
+            lt++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        Main T = new Main();
+        for (int x : T.solution(n, k, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
