44874cedac921b02f383f111761a14b78bd6bdf7
김홍식 <kimsikkong@gmail.com>
2021-05-01 22:03:44 +0900

    삽입 정렬 문제 풀이 개선

--- src/section_06_정렬_검색_알고리즘/s03_삽입_정렬/Main.java
+++ src/section_06_정렬_검색_알고리즘/s03_삽입_정렬/Main.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public int[] solution(int n, int[] arr) {
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j > 0 ; j--) {
-                if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
-                }
+        for (int i = 1; i < n; i++) {
+            int tmp = arr[i];
+            int j;
+            for (j = i - 1; j >= 0 ; j--) {
+                if (arr[j] > tmp) arr[j + 1] = arr[j];
+                else break;
             }
+            arr[j + 1] = tmp;
         }
 
         return arr;
