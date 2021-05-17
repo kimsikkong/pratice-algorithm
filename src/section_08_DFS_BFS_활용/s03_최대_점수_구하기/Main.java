2a8c4046a872d0623045fbe9727f83af8da2f578
김홍식 <kimsikkong@gmail.com>
2021-05-17 22:24:30 +0900

    [최대 점수 구하기] 문제 풀이 추가

---
+++ src/section_08_DFS_BFS_활용/s03_최대_점수_구하기/Main.java
@@ -0,0 +1,37 @@
+package section_08_DFS_BFS_활용.s03_최대_점수_구하기;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int answer;
+    public static int n;
+    public static int m;
+
+    public void DFS(int idx, int sum, int time, int[][] arr) {
+        if (time > m ) return;
+        else if (idx == n) answer = Math.max(answer, sum);
+        else {
+            answer = Math.max(answer, sum);
+            DFS(idx + 1, sum + arr[idx][0], time + arr[idx][1], arr);
+            DFS(idx + 1, sum, time, arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            arr[i][0] = a;
+            arr[i][1] = b;
+        }
+
+        Main T = new Main();
+        T.DFS(0, 0, 0, arr);
+        System.out.print(answer);
+    }
+}
