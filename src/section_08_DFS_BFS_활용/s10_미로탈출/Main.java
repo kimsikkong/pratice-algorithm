323a1ae6e5de376f5f100ef49344c4f88e2cfc9e
김홍식 <kimsikkong@gmail.com>
2021-05-29 00:38:10 +0900

    [미로탈출] 문제 풀이 추가

---
+++ src/section_08_DFS_BFS_활용/s10_미로탈출/Main.java
@@ -0,0 +1,43 @@
+package section_08_DFS_BFS_활용.s10_미로탈출;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static final int WIDTH = 7;
+    public static final int HEIGHT = 7;
+    public static int[] dx = {-1, 0, 1, 0};
+    public static int[] dy = {0, 1, 0, -1};
+    public static int[][] board = new int[WIDTH + 2][HEIGHT + 2];
+    public static int answer = 0;
+
+    public void DFS(int x, int y) {
+        if (x == WIDTH && y == HEIGHT) {
+            answer++;
+        } else {
+            for (int i = 0; i < dx.length; i++) {
+                int a = x + dx[i];
+                int b = y + dy[i];
+                if (a == 0 || b == 0 || a == WIDTH + 1 || b == HEIGHT + 1) continue;
+                if (board[a][b] == 0) {
+                    board[x][y] = 1;
+                    DFS(a, b);
+                    board[x][y] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i <= WIDTH; i++) {
+            for (int j = 1; j <= HEIGHT; j++) {
+                board[i][j] = scanner.nextInt();
+            }
+        }
+
+        Main T = new Main();
+        T.DFS(1, 1);
+        System.out.print(answer);
+    }
+}
