24a65549ba6063a542e69c1289b17a773a70eb8b
김홍식 <kimsikkong@gmail.com>
2021-05-30 18:25:46 +0900

    [토마토] 문제 개선 풀이 추가

--- src/section_08_DFS_BFS_활용/s12_토마토/Main.java
+++ src/section_08_DFS_BFS_활용/s12_토마토/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    class Point {
+    static class Point {
         public int x;
         public int y;
 
@@ -16,10 +16,12 @@ public class Main {
     }
 
     public static int[][] store;
+    public static int[][] days;
     public static int m;
     public static int n;
     public static int[] dx = {1, 0, -1, 0};
     public static int[] dy = {0, 1, 0, -1};
+    public static LinkedList<Point> Q;
 
     public boolean isCompleted() {
         for (int i = 0; i < n; i++) {
@@ -32,17 +34,8 @@ public class Main {
     }
 
     public int BFS() {
-        LinkedList<Point> Q = new LinkedList<>();
-        if (isCompleted()) return 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (store[i][j] == 1) Q.offer(new Point(i, j));
-            }
-        }
-
         if (Q.size() == 0) return -1;
-        int L = 0;
-        while (!Q.isEmpty() && !isCompleted()) {
+        while (!Q.isEmpty()) {
             int size = Q.size();
             for (int i = 0; i < size; i++) {
                 Point p = Q.poll();
@@ -51,17 +44,24 @@ public class Main {
                     int b = p.y + dy[j];
                     if (a >= 0 && a < n && b >= 0 && b < m && store[a][b] == 0) {
                         store[a][b] = 1;
+                        days[a][b] = days[p.x][p.y] + 1;
                         Q.offer(new Point(a, b));
                     }
                 }
             }
-
-            L++;
         }
 
         if (!isCompleted()) return -1;
+        else {
+            int answer = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    answer = Math.max(answer, days[i][j]);
+                }
+            }
 
-        return L;
+            return answer;
+        }
     }
 
     public static void main(String[] args) {
@@ -69,10 +69,12 @@ public class Main {
         m = scanner.nextInt();
         n = scanner.nextInt();
         store = new int[n][m];
-
+        days = new int[n][m];
+        Q = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 store[i][j] = scanner.nextInt();
+                if (store[i][j] == 1) Q.offer(new Point(i, j));
             }
         }
 
