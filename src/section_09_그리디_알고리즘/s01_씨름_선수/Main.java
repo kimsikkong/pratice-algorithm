a18c2a34136fc275d2027f5dddfcfd50656b1b32
김홍식 <kimsikkong@gmail.com>
2021-06-01 22:36:08 +0900

    [씨름 선수] 문제 풀이 추가

---
+++ src/section_09_그리디_알고리즘/s01_씨름_선수/Main.java
@@ -0,0 +1,51 @@
+package section_09_그리디_알고리즘.s01_씨름_선수;
+
+import java.util.*;
+
+public class Main {
+
+    static class Person implements Comparable {
+        public int h;
+        public int w;
+
+        public Person(int h, int w) {
+            this.h = h;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Person p = (Person) o;
+            return p.h - this.h;
+        }
+    }
+
+    public static List<Person> persons = new ArrayList<>();
+
+    public int solution(int n) {
+        int answer = 0;
+        Collections.sort(persons);
+        int maxHeight = persons.get(0).w;
+        for (Person p : persons) {
+            if (p.w >= maxHeight) {
+                maxHeight = p.w;
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            int h = scanner.nextInt();
+            int w = scanner.nextInt();
+            persons.add(new Person(h, w));
+        }
+
+        Main T = new Main();
+        System.out.print(T.solution(n));
+    }
+}
